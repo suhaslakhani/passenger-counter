e91123c198964e0a1948b732e513e24c1502dfcf
suhas <lakhanisuhas10@gmail.com>
2022-01-25 22:38:10 +0530

    extra code removed

--- passenger-counter.js
+++ passenger-counter.js
@@ -3,21 +3,15 @@ let countEL = document.getElementById("counter-el")
 
 let count = 0
 function increment() {
-    count = count + 1 
-    countEL.innerText = count
+    count = count + 1
+    countEL.textContent = count
 }
 let saveEl = document.getElementById("save-el")
-let previousEnteries = count + "-"
+
 function save() {
-    saveEl.innerText = previousEnteries
-    console.log(count)
+    let previousEnteries = count + " - "
+    saveEl.textContent += previousEnteries
+    countEL.textContent = 0
+    count = 0
+
 }
-
-
-// let welcomeEl = document.getElementById("welcome-el")
-// let Name = "suhas"
-// let greeting = "welcome back "
-
-//  welcomeEl.innerText = greeting + Name
-
-// // console.log(welcomeEL)
